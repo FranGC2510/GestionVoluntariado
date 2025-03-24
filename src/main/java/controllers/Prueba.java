71343ba4379be78e53d28e3244fa27eac640ba38
Francsico Javier García Cañero <franciscoj.garciacan@gmail.com>
2025-03-24 18:29:25 +0100

    Añadimos clases de prueba para que aparezcan el resto de paquetes.

---
+++ src/main/java/controllers/Prueba.java
@@ -0,0 +1,5 @@
+package controllers;
+
+public class Prueba {
+    /** Esto es como prueba pata el GitHub, borrar cuando comprobamos que a todos nos salen las mismas clases y paquetes.**/
+}
---
+++ src/main/java/interfaces/Prueba.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public class Prueba {
+    /** Esto es como prueba pata el GitHub, borrar cuando comprobamos que a todos nos salen las mismas clases y paquetes.**/
+}
